72557d4cba402ade8a5dd3a37ac970ebeddf41a6
Jaydon <pearcejaydon@gmail.com>
2022-09-04 23:17:47 +1000

    Add weather data object

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const APP_ID = "286b2e8186f06128e6e0f106f95dd6eb";
 
-async function getWeather(location) {
+async function getWeatherData(location) {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APP_ID}&units=metric`
     );
     const weatherData = await response.json();
-    console.log(weatherData);
+    const processedData = processWeatherData(weatherData);
+    console.log(processedData);
   } catch (error) {
     console.log(error);
   }
 }
 
-getWeather("Sydney");
+function processWeatherData(data){
+    const weather = {
+        condition: data['weather'][0]['description'],
+        temp: data['main']['temp'],
+        feelsLike: data['main']['feels_like'],
+        tempMin: data['main']['temp_min'],
+        tempMax: data['main']['temp_max'],
+        humidity: data['main']['humidity'],
+        name: data['name']
+
+    }
+    return weather;
+}
+getWeatherData("Sydney");
